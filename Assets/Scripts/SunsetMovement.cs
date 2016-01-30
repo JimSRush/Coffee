17c6ed0b0c05527272878fa946938d599fdb168a
JimSRush <jim@galesburg.co.nz>
2016-01-31 12:29:22 +1300

    Added retirement scene

---
+++ Assets/Scripts/SunsetMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunsetMovement : MonoBehaviour {
+	public float MovementTime;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Moving sunset");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.up * Time.deltaTime/180f, Space.World);
+		 
+		//this.transform.position.y = this.transform.position.y + (MovementTime * Time.deltaTime); 
+		//transform.Translate(new Vector2(Vector2.up)
+
+		//		transform.Translate(new Vector2(direction.y * Time.deltaTime * speed, direction.y * Time.deltaTime * speed));
+	}
+}
