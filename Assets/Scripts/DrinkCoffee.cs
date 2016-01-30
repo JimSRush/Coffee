9aaa495563d63d4cd81ce63af56084655612fef4
Mark Feaver <mark.feaver@gmail.com>
2016-01-30 21:50:08 +1300

    Added some SFX

--- Assets/Scripts/DrinkCoffee.cs
+++ Assets/Scripts/DrinkCoffee.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class DrinkCoffee : MonoBehaviour {
 
+	public AudioClip[] drinkingClips;
+	public AudioClip[] cupClips;
+
 	private Animator animator;
 	private bool isDrinking = false;
 
@@ -17,13 +20,25 @@ public class DrinkCoffee : MonoBehaviour {
 			Debug.Log("Total drunk: " + GameController.totalTimeSpentDrinkingCoffee);
 			//play slurp
 		}
-		if (Input.GetKeyUp("space")) {
+		else if (Input.GetKeyUp("space")) {
 			isDrinking = false;
 			animator.SetBool("IsDrinkingCoffee", isDrinking);
 			Debug.Log("Total drunk: " + GameController.totalTimeSpentDrinkingCoffee);
 		}
-		if (animator.GetCurrentAnimatorStateInfo(0).IsName("Paris_Drink_Coffee")) {
+		if (!animator.IsInTransition(0) && animator.GetCurrentAnimatorStateInfo(0).IsName("Paris_Drink_Coffee")) {
 			GameController.totalTimeSpentDrinkingCoffee += 1.0f * Time.deltaTime;
+			AudioSource audio = GetComponent<AudioSource>();
+	        if (!audio.isPlaying) {
+	        	audio.clip = drinkingClips[Random.Range(0, drinkingClips.Length)];
+	        	audio.Play();
+	        }
+		}
+		else if (animator.IsInTransition(0) && animator.GetNextAnimatorStateInfo(0).IsName("Paris_Idle")) {
+			AudioSource audio = GetComponent<AudioSource>();
+	        if (!audio.isPlaying) {
+	        	audio.clip = cupClips[Random.Range(0, cupClips.Length)];
+	        	audio.Play();
+	        }
 		}
 	}
 }
