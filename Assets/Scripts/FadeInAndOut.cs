d4d480cd53e1144268ccf19d5e4bf465aeaa8345
JimSRush <jim@galesburg.co.nz>
2016-01-30 14:45:13 +1300

    Markkyyyy

--- Assets/Scripts/FadeInAndOut.cs
+++ Assets/Scripts/FadeInAndOut.cs
@@ -4,21 +4,37 @@ using UnityEngine.UI;
 
 public class FadeInAndOut : MonoBehaviour {
 
-	public Image coveringPanel;
+	public Image coveringPanelFadeIn;
+	public Image coveringPanelFadeOut;
 	public float fadeTime;
 	Color toFadeTo;
 
 	// Use this for initialization
-	void Start () {
-		startFade ();
+//	void Start () {
+////		startFadeOut ();
+////		startFadeIn();
+//		startFadeOut();
+//	}
+	void Awake() {
+		startFadeOut ();
 	}
 
-	public void startFade() {
-		Color colorToFadeTo = new Color (1f, 1f, 1f, 0f);
-		coveringPanel.CrossFadeColor (colorToFadeTo, fadeTime, true, true);
+
+
+
+	void startFadeIn() {
+		Color colorToFadeInTo = new Color (0f, 0f, 0f, 0f);
+		coveringPanelFadeIn.CrossFadeColor (colorToFadeInTo, fadeTime, true, true);
 	}
 
-	
+	void startFadeOut() {
+
+		Color colorToFadeOutTo = new Color (0f, 1.0f, 0f, 0.9f);
+		coveringPanelFadeOut.alpha
+		coveringPanelFadeOut.CrossFadeColor (colorToFadeOutTo, fadeTime, true, true);
+//		coveringPanelFadeIn.color = colorToFadeOutTo;
+//		coveringPanelFadeIn.CrossFadeAlpha(1.0f, fadeTime * Time.deltaTime, false);
+	}
 
 
 }
