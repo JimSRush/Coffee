d9b02f45d41a2a29f6502d078179b426726ce314
JimSRush <jim@galesburg.co.nz>
2016-01-30 13:16:16 +1300

    Added working fade in script

--- Assets/Scripts/FadeInAndOut.cs
+++ Assets/Scripts/FadeInAndOut.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FadeInAndOut : MonoBehaviour {
 
+	public Image coveringPanel;
+	public float fadeTime;
+	Color toFadeTo;
+
 	// Use this for initialization
 	void Start () {
-	
+		startFade ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void startFade() {
+		Color colorToFadeTo = new Color (1f, 1f, 1f, 0f);
+		coveringPanel.CrossFadeColor (colorToFadeTo, fadeTime, true, true);
 	}
+
+	
+
+
 }
