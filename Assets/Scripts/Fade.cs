10d54d96b0be900a7ed52cd71a8750e2955267d6
JimSRush <jim@galesburg.co.nz>
2016-01-30 17:47:10 +1300

    Added a finally not useless method

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour {
+
+	void Start(){
+		FadeMe ();
+	}
+
+	public void FadeMe() {
+		StartCoroutine (DoFadeIn ());
+		Debug.Log ("Fading in");
+		//StartCoroutine (DoFadeOut ());
+		Debug.Log ("Fading out");
+
+	}
+
+	IEnumerator DoFadeIn(){
+		CanvasGroup cg = GetComponent<CanvasGroup> ();
+		while (cg.alpha > 0) {
+			cg.alpha -= Time.deltaTime / 2;
+			yield return null;
+		}
+		cg.interactable = false;
+		yield return null;
+	}
+
+	IEnumerator DoFadeOut() {
+		CanvasGroup cg = GetComponent<CanvasGroup> ();
+		cg.alpha = 0f;
+		while (cg.alpha < 255) {
+			cg.alpha += Time.deltaTime / 2;
+			yield return null;
+		}
+		cg.interactable = false;
+		yield return null;
+	}
+
+
+}
