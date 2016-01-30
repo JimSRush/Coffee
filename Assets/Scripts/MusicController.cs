4dd0c3de325fbce956704092b8fa40fba02c425d
Mark Feaver <mark.feaver@gmail.com>
2016-01-30 17:39:18 +1300

    Forgot to readd script

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour {
+
+	public AudioMixer masterMixer;
+	public AudioMixerSnapshot[] snapshots;
+	public float fadeTime = 10.0f;
+	private int currentSnapshot = 0;
+	private float[] timeGoals = new float[]{1.0f, 5.0f, 15.0f, 25.0f, 50.0f, 75.0f, 100.0f};
+
+	// Update is called once per frame
+	void Update () {
+		if(GameController.totalTimeSpentDrinkingCoffee > timeGoals[currentSnapshot]) {
+			NextSnapshot();
+		}
+	}
+
+	void NextSnapshot() {
+		currentSnapshot = (currentSnapshot + 1) % snapshots.Length;
+		AudioMixerSnapshot snapshot = snapshots[currentSnapshot];
+		snapshot.TransitionTo(fadeTime);
+	}
+}
