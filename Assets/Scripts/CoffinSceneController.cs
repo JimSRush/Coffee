0763f03140ff4f565c30f04c9694e3b955cb2b91
Mark Feaver <mark.feaver@gmail.com>
2016-01-31 14:34:05 +1300

    Added a controller to handle the coffin scene

---
+++ Assets/Scripts/CoffinSceneController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class CoffinSceneController : MonoBehaviour {
+
+	public float timeToWait = 8.0f;
+	private GameController gameController;
+	private float currentIdleTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		GameController.IsGameOver = true;
+		gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentIdleTime += Time.deltaTime;
+		if (currentIdleTime > timeToWait) {
+			ProceedToNextScene();
+		}
+	}
+
+	void ProceedToNextScene () {
+		// Proceed to next scene
+		gameController.StartCoroutine ("changeScene");
+	}
+}
--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -6,15 +6,24 @@ public class MusicController : MonoBehaviour {
 
 	public AudioMixer masterMixer;
 	public AudioMixerSnapshot[] snapshots;
+	public AudioMixerSnapshot gameoverSnapshot;
 	public float fadeTime = 10.0f;
 	private int currentSnapshot = 0;
 	private float[] timeGoals = new float[]{0.0f, 5.0f, 10.0f, 15.0f, 25.0f, 50.0f, 75.0f, 100.0f};
+	private bool isGameOver = false;
 
 	// Update is called once per frame
 	void Update () {
-		if(GameController.totalTimeSpentDrinkingCoffee >= timeGoals[currentSnapshot]) {
-			NextSnapshot();
-			Debug.Log("Transitioning to snapshot '" + snapshots[currentSnapshot].name + "'");
+		if (!GameController.IsGameOver) {
+			if(GameController.totalTimeSpentDrinkingCoffee >= timeGoals[currentSnapshot]) {
+				NextSnapshot();
+				Debug.Log("Transitioning to snapshot '" + snapshots[currentSnapshot].name + "'");
+			}
+		} else {
+			if (!isGameOver) {
+				isGameOver = true;
+				gameoverSnapshot.TransitionTo(fadeTime);
+			}
 		}
 	}
 
